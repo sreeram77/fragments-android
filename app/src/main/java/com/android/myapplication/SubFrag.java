ffba0d3750879159dde94d81befd330c77d7f15b
Sreeram Narayanan <sreeram.an1994@gmail.com>
2017-01-13 01:54:26 +0530

    Adding Files

---
+++ app/src/main/java/com/android/myapplication/SubFrag.java
@@ -0,0 +1,20 @@
+package com.android.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Sreeram AN on 13/01/2017.
+ */
+public class SubFrag extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.frag2, container, false);
+    }
+}
